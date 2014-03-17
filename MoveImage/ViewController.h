f8f0d97aea1225234e6241b2129a15739921f291
Xchobo <xchobo@gmail.com>
2014-03-18 00:32:40 +0800

    Initial Commit

---
+++ MoveImage/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MoveImage
+//
+//  Created by Xchobo on 2014/3/18.
+//  Copyright (c) 2014年 Xchobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
